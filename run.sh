5f447e7d21faa26b612afa85ce57b13a48d722e2
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-06-11 22:18:08 +0200

    Fixes

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 here="$HOME/.config/my-shell"
 . ./.venv/bin/activate
 . "$here/workflows/shared.sh"
-python "$here/init.py"
+
 pkill $app_name_lower
+python "$here/init.py"
 uwsm app -- python "$here/app.py" >/dev/null 2>&1 &
 disown
 
